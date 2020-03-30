8eb745b7db7bbc608eb2f09be96f2c5cd916f83c
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-03-30 20:47:38 +0530

    added navbar

--- src/core/Base.js
+++ src/core/Base.js
@@ -1,6 +1,33 @@
 import React from "react";
 import Menu from "./Menu";
 
-const Base = () => <div></div>;
+const Base = ({
+  title = "My Title",
+  description = "My Description",
+  className = "bg-light text-black p-4",
+  children
+}) => {
+  return (
+    <div>
+      <Menu />
+      <div className="container-fluid">
+        <div className="jumbotron text-black bg-light text-center">
+          <h2 className="display-4">{title}</h2>
+          <p className="lead">{description}</p>
+        </div>
+        <div className={className}>{children}</div>
+      </div>
+      <footer className="footer bg-light mt-auto py-3">
+        <div className="container-fluid bg-success text-white text-center py-3">
+          <h4>if you got any questions, feel free to reach out</h4>
+          <button className="btn btn-warning btn-lg">Contact us</button>
+        </div>
+        <div className="container">
+          <span className="text-muted">My Ecommerce</span>
+        </div>
+      </footer>
+    </div>
+  );
+};
 
 export default Base;
--- src/core/Home.js
+++ src/core/Home.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "../styles.css";
 import { API } from "../backend";
+import Base from "./Base";
 
 function Home() {
   // console.log("API IS ", API);
 
   return (
     <div>
-      <h1>Hello Front End</h1>
+      <Base title="Home Page">
+        <div className="row">
+          <div className="col-4">
+            <button className="btn btn-success">Test</button>
+          </div>
+          <div className="col-4">
+            <button className="btn btn-success">Test</button>
+          </div>
+          <div className="col-4">
+            <button className="btn btn-success">Test</button>
+          </div>
+        </div>
+      </Base>
     </div>
   );
 }
