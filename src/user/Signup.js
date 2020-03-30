0ca9e0682022dce1814d2426fb8902e1f8e4bf08
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-03-31 04:25:14 +0530

    added logic for signup

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,7 +1,46 @@
 import React, { useState } from "react";
 import Base from "../core/Base";
+import { signup } from "../auth/helper";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false
+  });
+
+  const { name, email, password, success, error } = values;
+
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const onSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password })
+      .then(data => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            success: true
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   const signUpForm = () => {
     return (
       <div className="row">
@@ -9,7 +48,13 @@ const Signup = () => {
           <form>
             <div className="form-group">
               <label className="text-dark">Name</label>
-              <input type="text" placeholder="Name" className="form-control" />
+              <input
+                type="text"
+                placeholder="Name"
+                className="form-control"
+                onChange={handleChange("name")}
+                value={name}
+              />
             </div>
             <div className="form-group">
               <label className="text-dark">Email</label>
@@ -17,6 +62,8 @@ const Signup = () => {
                 type="email"
                 placeholder="Email"
                 className="form-control"
+                onChange={handleChange("email")}
+                value={email}
               />
             </div>
             <div className="form-group">
@@ -25,10 +72,44 @@ const Signup = () => {
                 type="password"
                 placeholder="Password"
                 className="form-control"
+                onChange={handleChange("password")}
+                value={password}
               />
             </div>
-            <button className="btn btn-success btn-block">Submit</button>
+            <button className="btn btn-success btn-block" onClick={onSubmit}>
+              Submit
+            </button>
           </form>
+        </div>
+      </div>
+    );
+  };
+
+  const successMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-success"
+            style={{ display: success ? "" : "none" }}
+          >
+            New Account Created.Please <Link to="/signin">login Here</Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
         </div>
       </div>
     );
@@ -36,6 +117,8 @@ const Signup = () => {
 
   return (
     <Base title="Signup" description="User Signup">
+      {successMessage()}
+      {errorMessage()}
       {signUpForm()}
     </Base>
   );
