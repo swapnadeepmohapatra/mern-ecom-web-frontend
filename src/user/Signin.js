319d7528285f44d57b5ede9e155937445332de2d
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-04-20 23:49:55 +0530

    changed some routes

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -56,7 +56,7 @@ const Signin = () => {
 			if (user && user.role === 1) {
 				return <Redirect to="/admin/dashboard" />;
 			} else {
-				return <Redirect to="/user/dashboard" />;
+				return <Redirect to="/" />;
 			}
 		}
 		if (isAuthenticated()) {
