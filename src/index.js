5af6cbc78270cee2d63f3e2cb396a3ce5be6b799
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-04-25 12:04:39 +0530

    enabled PWA

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Routes from './Routes';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
+serviceWorker.register();
